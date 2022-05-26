c0c65d2ecf4014d89507399449049c279c3ed754
Aryan Jain <ajhunny.jain@gmail.com>
2022-05-26 10:13:29 +0530

    Babel config and package.json change

---
+++ dist/PrimaryButton/PrimaryButton.js
@@ -0,0 +1,54 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _core = require("@material-ui/core");
+
+var _excluded = ["variant", "size", "children", "style", "id", "className", "onClick", "disabled", "loading"];
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+var PrimaryButton = function PrimaryButton(_ref) {
+  var _ref$variant = _ref.variant,
+      variant = _ref$variant === void 0 ? 'contained' : _ref$variant,
+      _ref$size = _ref.size,
+      size = _ref$size === void 0 ? 'medium' : _ref$size,
+      children = _ref.children,
+      style = _ref.style,
+      id = _ref.id,
+      className = _ref.className,
+      onClick = _ref.onClick,
+      _ref$disabled = _ref.disabled,
+      disabled = _ref$disabled === void 0 ? false : _ref$disabled,
+      _ref$loading = _ref.loading,
+      loading = _ref$loading === void 0 ? false : _ref$loading,
+      props = _objectWithoutProperties(_ref, _excluded);
+
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement(_core.Button, _extends({
+    id: id,
+    className: className,
+    size: size,
+    onClick: onClick,
+    variant: variant,
+    disabled: disabled || loading,
+    disableElevation: true,
+    style: style
+  }, props), !loading ? children : /*#__PURE__*/_react.default.createElement(_core.CircularProgress, {
+    size: 20,
+    color: "primary"
+  })));
+};
+
+var _default = PrimaryButton;
+exports.default = _default;
---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "AutoComplete", {
+  enumerable: true,
+  get: function get() {
+    return _AutoComplete.default;
+  }
+});
+Object.defineProperty(exports, "PrimaryButton", {
+  enumerable: true,
+  get: function get() {
+    return _PrimaryButton.default;
+  }
+});
+
+var _PrimaryButton = _interopRequireDefault(require("./PrimaryButton/PrimaryButton"));
+
+var _AutoComplete = _interopRequireDefault(require("./AutoComplete/AutoComplete"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
