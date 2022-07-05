709dc911c627edc98b041ca77d201b0d59074f26
Aryan Jain <ajhunny.jain@gmail.com>
2022-07-05 15:05:33 +0530

    npm rc

--- dist/index.js
+++ dist/index.js
@@ -9,6 +9,12 @@ Object.defineProperty(exports, "AutoComplete", {
     return _AutoComplete.default;
   }
 });
+Object.defineProperty(exports, "OutlinedButton", {
+  enumerable: true,
+  get: function get() {
+    return _OutlinedButton.default;
+  }
+});
 Object.defineProperty(exports, "PrimaryButton", {
   enumerable: true,
   get: function get() {
@@ -19,5 +25,7 @@ Object.defineProperty(exports, "PrimaryButton", {
 var _PrimaryButton = _interopRequireDefault(require("./PrimaryButton/PrimaryButton"));
 
 var _AutoComplete = _interopRequireDefault(require("./AutoComplete/AutoComplete"));
+
+var _OutlinedButton = _interopRequireDefault(require("./OutlinedButton/OutlinedButton"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
